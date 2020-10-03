bc0a582d89fbea56db7f3307dacbefc715765718
Randeep Singh <randeepsaggu@gmail.com>
2020-10-03 17:16:39 +0530

    added code for NearestSmallest to right & Nearest greatest to left

--- Stack/Nearest.java
+++ Stack/Nearest.java
@@ -8,9 +8,58 @@ public class Nearest{
 		int arr[] = {1,3,5,24,52,5,2,52,5,25,3};
 		ArrayList<Integer> res = nearestGreatestRight( arr);
 		printArr("Original",arr);
-	
-		Collections.reverse(res);
+
 		printArr("Nearest Greatest to Right", res);
+		printArr("Nearest smallest to right", nearestSmallestRight(arr));
+		printArr("Nearest greatest to left", nearestGreatestLeft(arr));
+	}
+	static ArrayList<Integer> nearestGreatestLeft(int arr[])
+	{
+		ArrayList<Integer>al = new ArrayList<>();
+		Stack<Integer> st = new Stack<>();
+		for(int i = 0;i<arr.length;i++)
+		{
+			if(st.empty())
+				al.add(-1);
+			else if(!st.empty() && arr[i] < st.peek())
+				al.add(st.peek());
+			else if(!st.empty() && arr[i] >= st.peek())
+			{
+				while(!st.empty() && arr[i] >= st.peek())
+					st.pop();
+				if(st.empty())
+					al.add(-1);
+				else if(arr[i] < st.peek())
+					al.add(st.peek());
+			}
+			st.add(arr[i]);
+		}
+		return al;
+	}	
+	static ArrayList<Integer> nearestSmallestRight(int arr[])
+	{
+		ArrayList<Integer> al = new ArrayList<>();
+		Stack<Integer> st = new Stack<>();
+
+		for(int i=arr.length-1;i>=0;i--)
+		{
+			if(st.empty())
+				al.add(-1);
+			else if(!st.empty() && arr[i]>st.peek())
+				al.add(st.peek());
+			else if(!st.empty() && arr[i] <=st.peek())
+			{
+				while(!st.empty() && arr[i] <=st.peek())
+					st.pop();
+				if(st.empty())
+					al.add(-1);
+				else if(arr[i] > st.peek())
+					al.add(st.peek());
+			}
+			st.add(arr[i]);
+		}
+		Collections.reverse(al);
+		return al;
 	}
 	static ArrayList<Integer> nearestGreatestRight(int arr[])
 	{
@@ -28,7 +77,7 @@ public class Nearest{
 				al.add(st.peek());
 			}
 			else if(!st.empty() && arr[i]>=st.peek()){
-				while(!st.empty() && arr[i] > st.peek()){
+				while(!st.empty() && arr[i] >= st.peek()){
 					st.pop();
 				}
 				if(st.empty()){
@@ -42,7 +91,8 @@ public class Nearest{
 			st.add(arr[i]);
 		}
 
-		return al;	
+		Collections.reverse(al);	
+		return al;
 	}
 
 	static void printArr(String message, int[] arr)
@@ -56,7 +106,7 @@ public class Nearest{
 
 	}
 
-    static void printArr(String message, ArrayList<Integer> arr)
+	static void printArr(String message, ArrayList<Integer> arr)
 	{
 		System.out.println("");
 		System.out.println(message); 
