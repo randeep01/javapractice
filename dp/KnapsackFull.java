ff140463540909bc4aa2494888ab7639a84ad0a3
Randeep Singh <randeepsaggu@gmail.com>
2020-12-11 05:37:06 +0530

    updated knapsack memo

--- dp/KnapsackFull.java
+++ dp/KnapsackFull.java
@@ -10,8 +10,8 @@ public class KnapsackFull {
         int W = 50;
         int n = 3;
         //int[][] t = new int[n+1][W+1];
-        for(int i =0;i<n;i++){
-            for(int j =0;j<W;j++){
+        for(int i =0;i<=n;i++){
+            for(int j =0;j<=W;j++){
                 t[i][j] = -1;
             }
         }
@@ -39,9 +39,11 @@ public class KnapsackFull {
             return t[n][W];
         }
         if(wt[n-1] <= W){
-            return t[n][W] = Math.max(val[n-1] + knapsackMemo(wt, val, W-wt[n-1], n-1),knapsackMemo(wt, val, W, n-1));
+            t[n][W] = Math.max(val[n-1] + knapsackMemo(wt, val, W-wt[n-1], n-1),knapsackMemo(wt, val, W, n-1));
+            return t[n][W];
         } else {
-            return knapsackRec(wt, val, W, n-1);
+            t[n][W] = knapsackMemo(wt, val, W, n-1);
+            return t[n][W];
         }
     }
     static int knapsackDP(int[] wt, int[] val, int W, int n){
@@ -55,7 +57,6 @@ public class KnapsackFull {
                     dp[i][j] = Math.max(val[i-1] + dp[i-1][j-wt[i-1]],dp[i-1][j]);
                 } else {
                     dp[i][j] = dp[i-1][j];
-
                 }
             }
         }
